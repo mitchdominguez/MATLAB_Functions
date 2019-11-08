9a8864ae1e21eca8e6a9d55590e2d91462a67222
Mitch Dominguez <doming18@purdue.edu>
2019-11-08 02:16:09 -0500

    Fixed FPA code so it doesn't return NaNs when TA = 0 or 180 degrees

--- Orbits/fpa.m
+++ Orbits/fpa.m
@@ -16,7 +16,12 @@
 
 function FPA = fpa(r,v,mu,a,e,units)
     p = a*(1-e^2);
+    if abs(ta_conic(r,a,e,'deg')-180) <= 1e-12 || abs(ta_conic(r,a,e,'deg')) <= 1e-12 
+        FPA = 0;
+        return
+    end
     if strcmp(units,'deg')
+        sqrt(mu*p)/(r*v)
         FPA = acosd(sqrt(mu*p)/(r*v));
     else strcmp(units,'rad')
         FPA = acos(sqrt(mu*p)/(r*v));
