b547c6ca61735f9793129131b78ab96bca07dcb8
Mitch Dominguez <doming18@purdue.edu>
2019-11-15 10:00:44 -0500

    Fixed errors

--- Orbits/coe2rv.m
+++ Orbits/coe2rv.m
@@ -12,17 +12,48 @@
 %   mu = gravitational parameter [km3/s2]
 
 function [r_I, v_I, r_R, v_R] = rv2coe(coe,mu)
+    % Assume 3D orbital elements are zero if they are not given
+    if ~isfield(coe,'AOP')
+        coe.AOP = 0;
+    end
+    if ~isfield(coe,'RAAN')
+        coe.RAAN = 0;
+    end
+    if ~isfield(coe,'INC')
+        coe.INC = 0;
+    end
+
+    % Convet between using caps orbital elements and lowercase abbreviations
+    if ~isfield(coe,'SMA') && isfield(coe,'a')
+        coe.SMA = coe.a;
+    end
+    if ~isfield(coe,'ECC') && isfield(coe,'e')
+        coe.ECC = coe.e;
+    end
+    if ~isfield(coe,'TA') && isfield(coe,'thstar')
+        coe.TA = coe.thstar;
+    end
+    if ~isfield(coe,'AOP') && isfield(coe,'omega')
+        coe.AOP = coe.omega;
+    end
+    if ~isfield(coe,'RAAN') && isfield(coe,'Omega')
+        coe.RAAN = coe.Omega;
+    end
+    if ~isfield(coe,'INC') && isfield(coe,'i')
+        coe.INC = coe.i;
+    end
+
     a = coe.SMA;
     e = coe.ECC;
     thstar = coe.TA;
 
     % Rotation matrix from rotating to inertial frame
-    iCr = rot_rthh2inertial(coe.RAAN,coe.INC,coe.AOP+coe.TA);
+    iCr = rot_rthh2inertial(coe.RAAN,coe.INC,coe.AOP+coe.TA,'deg');
 
     % Calculate position
     r_mag = r_conic(thstar, a, e, 'deg');
     
-    r_R = r_mag*[cosd(thstar); sind(thstar); 0];
+    r_R = r_mag*[1; 0; 0];
     r_I = iCr*r_R;
 
     % Calculate velocity
@@ -31,6 +62,6 @@ function [r_I, v_I, r_R, v_R] = rv2coe(coe,mu)
     h_mag = sqrt(mu*p);
     gamma = sign(thstar)*abs(acosd(h_mag/(r_mag*v_mag)));
 
-    v_R = v_mag*[sind(thstar);cosd(thstar); 0];
+    v_R = v_mag*[sind(gamma);cosd(gamma); 0];
     v_I = iCr*v_R;
 end
