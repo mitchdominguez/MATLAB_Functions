51a46cc31ab7ac2553c7a0a5cefaf6cd9be05818
Mitch Dominguez <doming18@purdue.edu>
2020-09-23 02:06:04 -0400

    temp changes to kepler_time_NR.m

--- Orbits/kepler_time_NR.m
+++ Orbits/kepler_time_NR.m
@@ -4,19 +4,21 @@
 % Outputs:
 %   E_final = calculated eccentric anomaly [rad]
 %   err = error from NR iteration
+%   n = number of iterations
 % Inputs:
 %   M = mean anomaly [rad]
 %   e = eccentricity
 %   max_iter = maximum number of iterations allowed
 %   tol = convergence criteria
-function [E_final,err] = kepler_time_NR(M,e,max_iter,tol)
+function [E_final,err,n] = kepler_time_NR(M,e,max_iter,tol)
 
 % Calculate E0
-if (M/(1-e) < sqrt(6*(1-e)/e))
-    E(1) = M/(1-e);
-else
-    E(1) = (6*M/e)^(1/3);
-end
+E(1) = M;
+%if (M/(1-e) < sqrt(6*(1-e)/e))
+    %E(1) = M/(1-e);
+%else
+    %E(1) = (6*M/e)^(1/3);
+%end
 
 % Use Newton-Raphson to calculate E
 for i = 1:(max_iter-1)
@@ -32,5 +34,6 @@ for i = 1:(max_iter-1)
 end
 
 E_final = E(end);
+n = i;
 
 end
