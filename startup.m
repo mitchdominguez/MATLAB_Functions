694572dd20e406cdafb9befabd609f8ecadf30c0
Mitch Dominguez <doming18@purdue.edu>
2020-10-30 02:40:05 -0400

    Addpath for howell research

--- startup.m
+++ startup.m
@@ -12,3 +12,9 @@ set(groot, 'defaultLegendInterpreter','latex');
 set(0,'DefaultAxesFontSize',12)
 set(0,'DefaultAxesTitleFontSizeMultiplier',1.2)
 set(0,'DefaultAxesLabelFontSizeMultiplier',1.2)
+
+% Add entire research directory if opening from within Howell_Group directory
+if contains(pwd,'Research/Howell_Group')
+    research_path = genpath('/home/mitchdominguez/Research/Howell_Group/Code');
+    addpath(research_path);
+end
