c03eae2d0cb891b6b79dde419b997136acb39109
Mitch Dominguez <doming18@purdue.edu>
2021-06-22 16:13:08 -0400

    Added z fixed periodic orbit targeter

--- aa2dcm.m
+++ aa2dcm.m
@@ -2,7 +2,7 @@
 % Output a rotation matrix that actively rotates a vector about the
 % inputted axis and angle (specify units as 'rad' or 'deg'). Positive
 % angles result in a counterclockwise rotation
-function R = rotmataa(v,th,unit)
+function R = aa2dcm(v,th,unit)
     v = v/norm(v); % normalize axis
     if strcmp(unit,'deg')
         R = (cosd(th)*eye(3) + (1-cosd(th))*(v*v.') - sind(th)*crs(v)).';
