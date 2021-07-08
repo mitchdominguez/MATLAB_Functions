4b2c6e6b772dfc76a831e039199918822a1a6b1d
Mitch Dominguez <doming18@purdue.edu>
2021-07-08 10:51:07 -0500

    Fixed indexing error

--- R3BP/pair_eigs.m
+++ R3BP/pair_eigs.m
@@ -6,20 +6,21 @@
 function lam = pair_eigs(lam)
     % Find unit eigenvalues, which will be indices 5 and 6
     [~,ind5] = min(abs(lam-1));
-    lam5 = lam(ind5);
-    [~,ind6] = min(abs(lam(lam~=lam5)-1));
-    lam6 = lam(ind6);
+    lam5 = lam(ind5)
+    tlam0 = lam([1:ind5-1,ind5+1:end]);
+    [~,ind6] = min(abs(tlam0-1));
+    lam6 = tlam0(ind6)
 
     % Find first reciprocal pair 
     tlam = lam(lam~=lam5 & lam~=lam6);
-    lam1 = tlam(1);
+    lam1 = tlam(1)
     [~,ind2] = min(abs(tlam-1/lam1));
-    lam2 = tlam(ind2);
+    lam2 = tlam(ind2)
 
     % Find second reciprocal pair
     lam34 = tlam(tlam~=lam1 & tlam~=lam2);
-    lam3 = lam34(1);
-    lam4 = lam34(2);
+    lam3 = lam34(1)
+    lam4 = lam34(2)
 
     lam = [lam1;lam2;lam3;lam4;lam5;lam6];
 end
